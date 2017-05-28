d35c818b1fe5e524b673772432183ee7364b3b4d
bpglaser <bpglaser@gmail.com>
2017-05-28 15:58:56 -0400

    Begin seperation of grow and shrink

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -4,11 +4,11 @@ extern crate rmr;
 use std::fs::File;
 use std::path::Path;
 
-use image::{DynamicImage, ImageFormat};
+use image::{DynamicImage, GenericImage, ImageFormat};
 
 use rmr::BoxResult;
-use rmr::carve::{Carver, create_debug_image};
-use rmr::config::{Config, parse_args};
+use rmr::config::{Config, Mode, parse_args};
+use rmr::shrink::Shrinker;
 
 fn main() {
     parse_args().and_then(run).unwrap()
@@ -16,16 +16,52 @@ fn main() {
 
 fn run(config: Config) -> BoxResult<()> {
     let image = image::open(config.file_path)?;
-    let mut carver = Carver::new(&image);
 
-    let scaled_image = carver.resize(config.distance, config.orientation, config.mode);
-    save_image_to_path(&scaled_image, config.save_path)?;
+    let scaled_image = match config.mode {
+        Mode::Grow => grow(&image, config.distance),
+        Mode::Shrink => shrink(&image, config.distance),
+    };
 
-    if let Some(debug_image_path) = config.debug_image_path {
-        let debug_image = create_debug_image(&scaled_image, &carver.get_debug_points());
-        save_image_to_path(&debug_image, debug_image_path)?;
+    save_image_to_path(&scaled_image, config.save_path)
+}
+
+fn grow(image: &DynamicImage, distance: u32) -> DynamicImage {
+    unimplemented!()
+}
+
+fn shrink(image: &DynamicImage, distance: u32) -> DynamicImage {
+    let mut shrinker = Shrinker::new(&image);
+    let points = shrinker.calculate_removal_points(distance);
+    create_smaller_image(&image, distance, &points)
+}
+
+fn create_smaller_image(image: &DynamicImage,
+                        distance: u32,
+                        points: &[(u32, u32)])
+                        -> DynamicImage {
+
+    let (width, height) = image.dimensions();
+    let width = width - distance;
+
+    let mut smaller_image = DynamicImage::new_rgba8(width, height);
+    let mut new_x = 0;
+    let mut new_y = 0;
+
+    for (x, y, pixel) in image.pixels() {
+        if points.contains(&(x, y)) {
+            continue;
+        }
+
+        smaller_image.put_pixel(new_x, new_y, pixel);
+
+        new_x += 1;
+        if new_x == width {
+            new_x = 0;
+            new_y += 1;
+        }
     }
-    Ok(())
+
+    smaller_image
 }
 
 fn save_image_to_path<P: AsRef<Path>>(image: &DynamicImage, path: P) -> BoxResult<()> {
---
+++ src/shrink.rs
@@ -0,0 +1,13 @@
+use image::DynamicImage;
+
+pub struct Shrinker {}
+
+impl Shrinker {
+    pub fn new(image: &DynamicImage) -> Self {
+        unimplemented!()
+    }
+
+    pub fn calculate_removal_points(&mut self, distance: u32) -> Vec<(u32, u32)> {
+        unimplemented!()
+    }
+}
