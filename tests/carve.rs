98fddc2f1ab174b410cf6f3c878db6681757321c
bpglaser <bpglaser@gmail.com>
2017-06-09 21:33:48 -0400

    Added path_energy tests

--- tests/carve.rs
+++ tests/carve.rs
@@ -7,7 +7,6 @@ macro_rules! setup_carver {
     ( $bytes:expr ) => {
         {
             let input = image::load_from_memory($bytes).unwrap();
-
             let mut carver = Carver::new(&input);
             carver.calculate_energy();
             carver
@@ -16,33 +15,62 @@ macro_rules! setup_carver {
 }
 
 #[test]
-fn carver_small_energy_test() {
+fn carver_small_pixel_energy_test() {
     let carver = setup_carver!(SMALL);
-    let energy = carver.get_pixel_energy();
-    assert_eq!(get_small_energy(), energy);
+    let pixel_energy = carver.get_pixel_energy();
+    assert_eq!(get_small_pixel_energy(), pixel_energy);
 }
 
 #[test]
-fn carver_medium_energy_test() {
+fn carver_small_path_energy_test() {
+    let carver = setup_carver!(SMALL);
+    let path_energy = carver.get_path_energy();
+    assert_eq!(get_small_path_energy(), path_energy);
+}
+
+#[test]
+fn carver_medium_pixel_energy_test() {
     let carver = setup_carver!(MEDIUM);
-    let energy = carver.get_pixel_energy();
-    assert_eq!(get_medium_energy(), energy);
+    let pixel_energy = carver.get_pixel_energy();
+    assert_eq!(get_medium_pixel_energy(), pixel_energy);
+}
+
+#[test]
+fn carver_medium_path_energy_test() {
+    let carver = setup_carver!(MEDIUM);
+    let path_energy = carver.get_path_energy();
+    assert_eq!(get_medium_path_energy(), path_energy);
 }
 
 static SMALL: &'static [u8; 173] = include_bytes!("images/small_energy.png");
 static MEDIUM: &'static [u8; 244] = include_bytes!("images/medium_energy.png");
 
-fn get_small_energy() -> Vec<Vec<u32>> {
+fn get_small_pixel_energy() -> Vec<Vec<u32>> {
     vec![vec![20808, 52020, 20808],
          vec![20808, 52225, 21220],
          vec![20809, 52024, 20809],
          vec![20808, 52225, 21220]]
 }
 
-fn get_medium_energy() -> Vec<Vec<u32>> {
+fn get_small_path_energy() -> Vec<Vec<u32>> {
+    vec![vec![20808, 52020, 20808],
+         vec![41616, 73033, 42028],
+         vec![62425, 93640, 62837],
+         vec![83233, 114650, 84057]]
+}
+
+fn get_medium_pixel_energy() -> Vec<Vec<u32>> {
     vec![vec![57685, 50893, 91370, 25418, 33055, 37246],
          vec![15421, 56334, 22808, 54796, 11641, 25496],
          vec![12344, 19236, 52030, 17708, 44735, 20663],
          vec![17074, 23678, 30279, 80663, 37831, 45595],
          vec![32337, 30796, 4909, 73334, 40613, 36556]]
+}
+
+fn get_medium_path_energy() -> Vec<Vec<u32>> {
+    vec![vec![57685, 50893, 91370, 25418, 33055, 37246],
+         vec![66314, 107227, 48226, 80214, 37059, 58551],
+         vec![78658, 67462, 100256, 54767, 81794, 57722],
+         vec![84536, 91140, 85046, 135430, 92598, 103317],
+         vec![116873, 115332, 89955, 158380, 133211, 129154]]
 }
